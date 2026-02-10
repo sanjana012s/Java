dab75c48b0b447cb120e4899d825e31f50ca291e
sanjana012s <sanjana081220@gmail.com>
2026-02-10 11:23:19 +0530

    added sorting concept

---
+++ lec11_sorting/insertion.java
@@ -0,0 +1,22 @@
+class insertion{
+  public static void array(int arr[]){
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+  public static void main(String args[]){
+    int arr[]={12,22,2,2,11};
+    // insertion sort
+    for(int i=1;i<arr.length;i++){
+      int current=arr[i];
+      int j=i-1;
+      while(j>=0 && current < arr[j]){
+        arr[j+1]=arr[j];
+        j--;
+      }
+      // placement
+      arr[j+1]=current;
+    }
+    array(arr);
+  }
+}
---
+++ lec11_sorting/selection.java
@@ -0,0 +1,27 @@
+class selection{
+  //function
+  public static void array(int arr[]){
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+
+public static void main(String args[]){
+  int arr[]={12,2,3,4};
+  System.out.println(arr.length);
+  // selection sort
+  for(int i=0;i<arr.length-1;i++){
+    int smallest=i;
+    for(int j=i+1;j<arr.length;j++){
+      if(arr[smallest]>arr[j]){
+        smallest=j;
+      }
+    }
+    int temp=arr[smallest];
+    arr[smallest]=arr[i];
+    arr[i]=temp;
+  }
+  array(arr);
+}
+
+}
